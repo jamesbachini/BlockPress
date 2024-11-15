fb1eebde2d2ebcae6420bc9232047c4df38b1213
Anon <anon@tmp.com>
2024-11-15 19:51:10 +0700

    test working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Module from './Module';
+import Post from './Post';
+import Docs from './Docs';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/module" element={<Module />} />
+      <Route path="/post" element={<Post />} />
+      <Route path="/docs" element={<Docs />} />
+    </Routes>
   );
-}
+};
 
 export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure all React files are scanned
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        domine: ['Domine', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
