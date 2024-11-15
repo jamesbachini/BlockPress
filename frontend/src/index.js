433f8545ae90898b3b76afbbfe410d0b4edc5556
Anon <anon@tmp.com>
2024-11-16 06:56:05 +0700

    rebuild docs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,9 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const basename = process.env.NODE_ENV === 'production' ? '/BlockPress' : '';
 root.render(
-  <HashRouter>
+  <HashRouter basename={basename}>
     <App />
   </HashRouter>,
 );
